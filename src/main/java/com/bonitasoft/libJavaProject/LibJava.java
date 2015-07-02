68fb2596296c19bff6515e84823094dbb72816e4
happykiller <fabrice.rosito@gmail.com>
2015-07-02 12:16:50 +0200

    update repo bdm

--- src/main/java/com/bonitasoft/libJavaProject/LibJava.java
+++ src/main/java/com/bonitasoft/libJavaProject/LibJava.java
@@ -14,14 +14,35 @@ import java.util.logging.Logger;
 public class LibJava {
 
     private static final Logger uilLogger = Logger.getLogger("com.bonitasoft.groovy");
+	
+	public static void trace(String message){
+        try {
+            uilLogger.info(message);
+            System.out.println(message);
+        }catch (Exception ex) {
+            uilLogger.severe("trace - Error : " + ex);
+        }
+    }
 
     public static String sayHelloMessage(){
         String retour = "HelloWorld";
         try {
-            uilLogger.fine("sayHelloMessage - msg : HelloWorld.");
             return retour;
         }catch (Exception e) {
-            uilLogger.fine("sayHelloMessage - Error : "+ e.getMessage());
+            trace("sayHelloMessage - Error : "+ e.getMessage());
+            return null;
+        }
+    }
+	
+    public static String getDateTimeStr(){
+        try {
+            Calendar calendar = Calendar.getInstance();
+            java.util.Date currentDate = calendar.getTime();
+            java.sql.Date dateReturn = new java.sql.Date(currentDate.getTime());
+            SimpleDateFormat formater = new SimpleDateFormat("yyyyMMddHHmmss");
+            return formater.format(dateReturn);
+        }catch (Exception ex) {
+            trace("getDateTimeStr - Error : " + ex);
             return null;
         }
     }
